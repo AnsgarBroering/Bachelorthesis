9b041077f53f5276cfe3843f0091a5d7af36adfd
Ansgar Bröring <ansgar.broering@uni-oldenburg.de>
2023-05-23 11:07:36 +0200

    Starting programming now.

---
+++ Multiagent_Coalition/Agents/Main.py
@@ -0,0 +1,4 @@
+import asyncio
+
+class __main__:
+    input()
---
+++ Multiagent_Coalition/Agents/Multi_Agent_System.py
@@ -0,0 +1,6 @@
+
+coordinate_size_x = 3
+coordinate_size_y = 2
+agents = []
+
+class Multi_Agent_System(coordinate_size_x,coordinate_size_x,):
